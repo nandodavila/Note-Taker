b011016879c947c710960814ad9440aa81a42185
Nando <nandodavila94@gmail.com>
2021-11-04 19:14:58 -0400

    yeah..... I have no idea what to do, im gonna rewatch all classes on  express

---
+++ server.js
@@ -0,0 +1,60 @@
+const express = require('express');
+const path = require('path');
+const api = require('./routes/index.js');
+const notes = require('./db/db.json');
+
+const PORT = process.env.PORT || 3001;
+
+const app = express();
+
+
+// Middleware for parsing JSON and urlencoded form data
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use('/api', api);
+
+app.use(express.static('public'));
+
+
+app.get('/', (req, res) =>
+  res.sendFile(path.join(__dirname, '/public/index.html'))
+);
+
+app.get('/notes', (req, res) =>
+  res.sendFile(path.join(__dirname, '/public/notes.html'))
+);
+
+app.post('/notes', (req, res) => {
+  // Log that a POST request was received
+  console.info(`${req.method} request received to add a note`);
+
+  // Prepare a response object to send back to the client
+  const { title, text} = req.body;
+
+  // If all the required properties are present
+  if (title && text) {
+    // Variable for the object we will save
+    const newNote = {
+      title,
+      text,
+    };
+
+    const response = {
+      status: 'success',
+      body: newNote,
+    };
+
+    console.log(response);
+    res.status(201).json(response);
+  } else {
+    res.status(500).json('Error in posting review');
+  }
+});
+
+app.get('*', (req, res) =>
+  res.sendFile(path.join(__dirname, 'public/index.html'))
+);
+
+app.listen(PORT, () =>
+  console.log(`App listening at http://localhost:${PORT} 🚀`)
+);
